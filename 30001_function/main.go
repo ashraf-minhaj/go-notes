3bc92da9de6e84d203efb205c4cecd900d7114be
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2023-03-03 02:56:36 +0600

    function

---
+++ 30001_function/main.go
@@ -0,0 +1,31 @@
+/* different things with a function
+* author: ashraf minhaj
+* mail  : ashraf_minhaj@yahoo.com
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func multiply(num1 int, num2 int) (int, error) {
+	/* handle error, and return using a function */
+	if num1 < 0 || num2 < 0 {
+		return 0, fmt.Errorf("The value can not be negative, why? My boss does not like 'em.")
+	}
+	result := num1 * num2
+	return result, nil
+}
+
+func main() {
+	// call the function
+	answer, err := multiply(-2, 3)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(answer)
+}
